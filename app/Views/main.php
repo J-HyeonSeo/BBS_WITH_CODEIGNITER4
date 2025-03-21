42d464a0651b70231108227dcaccfd3bb57e7afe
J-Hyeonseo <47245112+J-HyeonSeo@users.noreply.github.com>
2025-03-21 16:55:06 +0900

    Initial commit

---
+++ app/Views/main.php
@@ -0,0 +1,92 @@
+<main>
+
+    <!-- 갯수 표시 및 검색창 -->
+    <div id="bbs-header">
+        <h4>총 12개</h4>
+        <div id="search">
+            <input placeholder="검색어를 입력해주세요..">
+            <button></button>
+        </div>
+    </div>
+    <hr>
+    <!-- 게시글 내용 순회 -->
+    <ol id="bbs-content">
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+        <li class="bbs-row">
+            <h4 class="number">12</h4>
+            <a href="/bbs/12" class="title">오... CodeIgniter 매우 혁신적인 프레임워크인데..?</a>
+            <h4 class="view">[ 34읽음 ]</h4>
+            <h4 class="writer">개발자J군</h4>
+        </li>
+    </ol>
+
+    <!-- 페이지네이션 + 하단 -->
+    <div id="bbs-bottom">
+        <ol id="bbs-page">
+            <a href="?page=1">&lt;&lt;</a>
+            <a href="?page=1">&lt;</a>
+            <a href="?page=1">1</a>
+            <a href="?page=1">2</a>
+            <a href="?page=1">3</a>
+            <a href="?page=1">4</a>
+            <a href="?page=1">5</a>
+            <a href="?page=1">&gt;</a>
+            <a href="?page=1">&gt;&gt;</a>
+        </ol>
+        <a href="/bbs/new" class="btn" id="new-form-btn">+ 작성</a>
+    </div>
+
+</main>
---
+++ app/Views/templates/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <!-- css -->
+    <link rel="stylesheet" href="public/css/styles.css">
+    
+    <!-- js --->
+    
+    <title>PHP & CodeIgniter 게시판</title>
+</head>
+<body>
+    <header>
+        <!-- 제목 스타일링 -->
+        <div id="title">
+            <h1>PHP & CodeIgniter</h1>
+            <h1>게시판</h1>
+        </div>
+        
+        <!-- 로그인 -->
+        <div class="btn" id="login">
+            <a href="/login">로그인</a>
+        </div>
+        
+        <!-- 사용자명 + 로그아웃 -->
+<!--        <h4 id="nickname">개발자J군</h4>-->
+<!--        <div class="btn" id="logout">-->
+<!--            <a href="/logout">로그아웃</a>-->
+<!--        </div>-->
+
+    </header>
